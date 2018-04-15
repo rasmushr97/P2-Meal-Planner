bd1bf77676d1d46a0c53d308bde57698c26ce9c8
Rasmus <rasmushr97@gmail.com>
2018-04-15 13:21:20 +0200

    Added back buttons on the pages that needed plus a little cleanup

--- app/src/main/java/com/example/rasmus/menucomplete/AppDrawerActivity.java
+++ app/src/main/java/com/example/rasmus/menucomplete/AppDrawerActivity.java
@@ -5,6 +5,7 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -18,25 +19,29 @@ import com.example.rasmus.menucomplete.UI.activities.MainActivity;
 
 public abstract class AppDrawerActivity extends AppCompatActivity implements MenuItem.OnMenuItemClickListener {
     private FrameLayout view_stub; //This is the framelayout to keep your content view
-    private NavigationView navigation_view; // The new navigation view from Android Design Library. Can inflate menu resources. Easy
-    private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
-    private Menu drawerMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.app_base_layout);// The base layout that contains your navigation drawer.
+
+        Menu drawerMenu;
+        NavigationView navigation_view = (NavigationView) findViewById(R.id.navigation_view);
+        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
         view_stub = (FrameLayout) findViewById(R.id.view_stub);
-        navigation_view = (NavigationView) findViewById(R.id.navigation_view);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, 0, 0);
         //mDrawerLayout.setDrawerListener(mDrawerToggle);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         drawerMenu = navigation_view.getMenu();
-        for(int i = 0; i < drawerMenu.size(); i++) {
-          drawerMenu.getItem(i).setOnMenuItemClickListener(this);
+        for (int i = 0; i < drawerMenu.size(); i++) {
+            drawerMenu.getItem(i).setOnMenuItemClickListener(this);
         }
         // and so on...
     }
